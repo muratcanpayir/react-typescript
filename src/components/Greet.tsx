ce9810b09a5c21432482617127ad87a3d6d657b7
Muratcan Payir <muratcanpayir@gmail.com>
2021-12-25 12:22:17 +0000

    string props

---
+++ src/components/Greet.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+type GreetProps={
+  name:string
+}
+
+function Greet(props:GreetProps) {
+  return (
+    <div>
+      <h2>Welcome {props.name}! You have 10 unread messages!</h2> 
+    </div>
+  )
+}
+
+export default Greet
